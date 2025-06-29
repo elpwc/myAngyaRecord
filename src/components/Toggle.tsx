e113b2dd42ca69001e49af11ff2e2873ae2c6f7b
elpwc/wniko <elpwc@hotmail.com>
2025-06-29 15:14:08 +0900

    feat: Add size to Toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -6,14 +6,26 @@ type ToggleSwitchProps = {
   onChange: (value: boolean) => void;
   disabled?: boolean;
   className?: string;
+  size?: number;
   style?: React.CSSProperties;
 };
 
-const Toggle: React.FC<ToggleSwitchProps> = ({ value, onChange, disabled = false, className = '', style = {} }) => {
+const WIDTH = 18;
+
+const Toggle: React.FC<ToggleSwitchProps> = ({ value, onChange, disabled = false, className = '', size = 1, style = {} }) => {
   return (
     <label
       className={`toggle-switch ${disabled ? 'disabled' : ''} ${className}`}
-      style={style}
+      style={{
+        ...style,
+        ['--width' as any]: `${WIDTH * 2 * size}px`,
+        ['--height' as any]: `${(WIDTH + 2) * size}px`,
+        ['--translateX' as any]: `${(WIDTH - 2) * size}px`,
+        ['--buttonWidth' as any]: `${WIDTH * size}px`,
+        ['--buttonHeight' as any]: `${WIDTH * size}px`,
+        ['--buttonLeft' as any]: `${1 * size}px`,
+        ['--buttonTop' as any]: `${1 * size}px`,
+      }}
       onClick={e => {
         e.stopPropagation();
       }}
