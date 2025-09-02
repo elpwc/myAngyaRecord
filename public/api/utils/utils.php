1a1523e757b70d600eaf633b05f0c7bfbdaec925
elpwc <elpwc@hotmail.com>
2025-09-03 02:28:32 +0900

    server: deleted old mail sending code

--- public/api/utils/utils.php
+++ public/api/utils/utils.php
@@ -6,7 +6,6 @@
  */
 
 require dirname(__FILE__) . '/../private/illegal_words_list.php';
-require dirname(__FILE__) . '/../plugin/Lib_Smtp.php';
 require dirname(__FILE__) . '/../utils/mailSender.php';
 
 /**
@@ -74,27 +73,6 @@ function cator_to_cn_censorship($text)
     $res = str_ireplace($word, str_repeat('*', mb_strlen($word)), $res);
   }
   return $res;
-}
-
-
-
-function send_verification_code_mail($target, $verify_code)
-{
-  try {
-    $mail = new Lib_Smtp();
-
-    $mail->setServer(EMAIL_HOST, EMAIL_USER, EMAIL_PASS, EMAIL_PORT, true);
-    $mail->setFrom(EMAIL_MAIL);
-    $mail->setReceiver($target);
-    $mail->addAttachment("");
-    $mail->setMail(
-      "邮箱验证码",
-      '<h3>验证码是：<span>' . $verify_code . '</span></h3><p>有效期：5分钟</p>' . date('Y-m-d H:i:s')
-    );
-    return true;
-  } catch (Exception $e) {
-    return false;
-  }
 }
 
 
