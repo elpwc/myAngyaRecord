51e5d0f2cfc8b5aa9769bd9b921f2be2d1b25008
elpwc/wniko <elpwc@hotmail.com>
2025-09-16 20:10:19 +0900

    feat: add more mapStyles

--- src/utils/mapStyles.ts
+++ src/utils/mapStyles.ts
@@ -1,5 +1,14 @@
 export const mapStyles = [
-  { name: 'classic', bgcolor: ['white', '#bef7ff', '#50ff50', '#ffa136', '#ff3d3d', '#d646d6'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
-  { name: 'light', bgcolor: ['white', '#ceeaff', '#a5ffa5', '#ffdf72', '#ff8686', '#ffabff'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
-  { name: 'dark', bgcolor: ['white', '#72c4ff', '#43dd43', '#faa429', '#ff3d3d', '#d646d6'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
+  { name: 'classic', title: 'クラシック', bgcolor: ['white', '#bef7ff', '#50ff50', '#ffa136', '#ff3d3d', '#d646d6'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
+  { name: 'default', title: 'デフォルト', bgcolor: ['white', '#72c4ff', '#43dd43', '#faa429', '#ff3d3d', '#d646d6'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
+  { name: 'light', title: '淡色', bgcolor: ['white', '#ceeaff', '#a5ffa5', '#ffdf72', '#ff8686', '#ffabff'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
+  { name: 'dark', title: '暗色', bgcolor: ['white', '#367099ff', '#298829ff', '#ac772dff', '#974545ff', '#973397ff'], color: ['black', 'white', 'white', 'white', 'white', 'white'] },
+  { name: 'redgreen', title: '緑＆赤', bgcolor: ['white', '#8bc34a', '#cddc39', '#ff9800', '#ff5722', '#d64689ff'], color: ['black', 'black', 'black', 'black', 'black', 'black'] },
+  { name: 'gray', title: '灰色', bgcolor: ['white', '#cfcfcfff', '#929292ff', '#696969ff', '#363636ff', '#181818ff'], color: ['black', 'black', 'black', 'white', 'white', 'white'] },
+  {
+    name: 'colorblind',
+    title: '色覚異常対応テーマ',
+    bgcolor: ['white', 'rgb(100, 143, 255)', 'rgb(120, 94, 240)', 'rgb(255, 176, 0)', 'rgb(254, 97, 0)', 'rgb(220, 38, 127)'],
+    color: ['black', 'black', 'black', 'black', 'black', 'black'],
+  },
 ];
