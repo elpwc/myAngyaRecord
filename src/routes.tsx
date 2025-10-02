efda9f78a3e9e66e681c6c7112f00037bfd031d7
elpwc/wniko <elpwc@hotmail.com>
2025-10-02 17:15:29 +0900

    code: Move routes out from App.tsx

---
+++ src/routes.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Navigate, Route, Routes } from 'react-router';
+import ErrorPage from './pages/ErrorPage';
+import Japan from './pages/Japan';
+import Main from './pages/Main';
+import Numazu from './pages/Numazu';
+import Login from './components/userSysCompo/Login';
+import EmailVerify from './components/userSysCompo/EmailVerify';
+import Register from './components/userSysCompo/Register';
+import ResetPassword from './components/userSysCompo/ResetPassword';
+import Ranking from './pages/Ranking';
+import { MapsId } from './utils/map';
+import Vietnam from './pages/Vietnam';
+import UserPage from './pages/UserPage';
+
+export const AppRoutes = () => {
+  const [openMobileAsideMenu, setopenMobileAsideMenu] = useState(true);
+  return (
+    <Routes>
+      <Route
+        path=""
+        element={
+          <Main
+            onMobileMenuClick={() => {
+              setopenMobileAsideMenu(!openMobileAsideMenu);
+            }}
+          />
+        }
+      >
+        <Route index element={<Navigate to="/japan" replace />} />
+        <Route path="japan" element={<Japan openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+        <Route path="japan/:id" element={<Japan openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+        <Route path="numazu" element={<Numazu openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+        <Route path="numazu/:id" element={<Numazu openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+        <Route path="vietnam" element={<Vietnam openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+        <Route path="vietnam/:id" element={<Vietnam openMobileAsideMenu={openMobileAsideMenu} />}></Route>
+
+        <Route path="ranking">
+          <Route index element={<Navigate to="japan" replace />} />
+          <Route path="japan" element={<Ranking mapId={MapsId.JapanMuni} />}></Route>
+          <Route path="numazu" element={<Ranking mapId={MapsId.Numazu} />}></Route>
+          <Route path="vietnam" element={<Ranking mapId={MapsId.Vietnam} />}></Route>
+        </Route>
+
+        <Route path="login" element={<Login />}></Route>
+        <Route path="emailverify" element={<EmailVerify />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="resetpassword" element={<ResetPassword />}></Route>
+
+        <Route path="user" element={<UserPage />}></Route>
+        <Route path="user/:id" element={<UserPage />}></Route>
+
+        <Route path="404" element={<ErrorPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Route>
+    </Routes>
+  );
+};
